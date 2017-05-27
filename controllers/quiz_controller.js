4b0305b44b0c0a444a1180293b556c9220675847
anita1602 <amartinezestada@gmail.com>
2017-05-27 21:07:09 +0200

    juego

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -235,6 +235,8 @@ exports.randomcheck = function (req, res, next) {
    
    if(result){
 	req.session.score++;
+   }else{
+	req.session.score=0;
    }
    res.render('quizzes/random_result', {
 	score: req.session.score,
